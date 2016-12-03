91cdeca3f04d6c5eb744ac8a0b613e9d3c56e42c
lagoon92 <lagoon92.sk@gmail.com>
2016-12-03 03:58:14 +0100

    Add files via upload

--- CSORepository/src/CSO.java
+++ CSORepository/src/CSO.java
@@ -1,19 +1,34 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class CSO extends JFrame implements ActionListener {
+	JButton bPodajDate, bWyjscie;
+	JLabel lWyswietlDate;
+	
 	public CSO(){
 		setSize(300,200);
 		setTitle("CSORepo");
 		setLayout(null);
-		JButton button = new JButton("Podaj datê");
-		button.setBounds(0, 0, 100, 20);
-		add(button);
-		button.addActionListener(this);
+		bPodajDate = new JButton("Podaj datê");
+		bPodajDate.setBounds(0, 0, 100, 20);
+		add(bPodajDate);
+		bPodajDate.addActionListener(this);
+		
+		bWyjscie = new JButton("Wyjœcie");
+		bWyjscie.setBounds(0, 50, 100, 20);
+		add(bWyjscie);
+		bWyjscie.addActionListener(this);
+		
+		lWyswietlDate = new JLabel("Data: ");
+		lWyswietlDate.setBounds(0, 120, 200, 20);
+		lWyswietlDate.setForeground(Color.PINK);
+		add(lWyswietlDate);
 	}
 	public static void main(String[]args){
 		CSO okno = new CSO();
@@ -23,7 +38,14 @@ public class CSO extends JFrame implements ActionListener {
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println(new Date());
+		Object zrodlo = e.getSource();
+		if (zrodlo==bPodajDate){
+			lWyswietlDate.setText(new Date().toString());
+			//System.out.println(new Date());	
+		}
+		else if (zrodlo==bWyjscie){
+			dispose();
+		}
 		
 	}
 }
